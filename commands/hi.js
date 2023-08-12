32a4cc5125211ed4e37bf98272c7413ad0bced52
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-08-12 10:08:51 +0500

    refactor: subfolders for command types

--- commands/hi.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-
-export const data = new SlashCommandBuilder()
-  .setName("hi")
-  .setDescription("Replies with Hey!");
-
-export async function execute(interaction) {
-  await interaction.reply("Hey!");
-}
